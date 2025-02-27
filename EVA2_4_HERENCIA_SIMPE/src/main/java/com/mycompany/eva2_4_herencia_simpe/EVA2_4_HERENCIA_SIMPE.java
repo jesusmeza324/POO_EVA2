92d4b3b396cb08c2982bd8cf1123d2f4ad356869
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2025-02-27 17:19:27 -0600

    Add files via upload

---
+++ EVA2_4_HERENCIA_SIMPE/src/main/java/com/mycompany/eva2_4_herencia_simpe/EVA2_4_HERENCIA_SIMPE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_4_herencia_simpe;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_4_HERENCIA_SIMPE {
+
+    public static void main(String[] args) {
+        
+    }
+}
